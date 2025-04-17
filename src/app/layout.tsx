50ef7b8b0090e5a8a4fc286191d20a3351143b99
BennyPLS <peptoni0345@gmail.com>
2025-04-17 15:02:45 +0200

    Add PWA support with manifest file and updated icons
    Added a web app manifest for PWA functionality, including metadata like icons, theme colors, and display mode. Updated `layout.tsx` to include various favicon options and related meta tags for better support across platforms.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,15 +13,23 @@ import FirstTimeVisitScript from '~/scripts/first-time-visit'
 export const metadata: Metadata = {
     title: 'Pomodoro Timer',
     description: 'Simple pomodoro timer',
-    icons: [{ rel: 'icon', url: '/favicon.ico' }],
+    icons: [
+        { rel: 'icon', type: 'shortcut icon', url: '/favicon.ico' },
+        { rel: 'icon', url: '/favicon.ico' },
+        { rel: 'icon', type: 'image/svg+xml', url: '/favicon.svg' },
+        { rel: 'icon', type: 'image/png', url: '/favicon-96x96.png', sizes: '96x96' },
+        { rel: 'apple-touch-icon', sizes: '180x180', url: '/apple-icon.png' },
+    ],
 }
 
 export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
     return (
         <html suppressHydrationWarning lang="es" className={`${GeistSans.variable}`}>
             <head>
+                <meta name="apple-mobile-web-app-title" content="Pomodoro" />
                 <Script crossOrigin="anonymous" src="//unpkg.com/react-scan/dist/auto.global.js" />
                 <FirstTimeVisitScript />
+                <title>Pomodoro Timer</title>
             </head>
             <body>
                 <Toaster richColors={true} />
