ae6bd8bfe35a3161f25051996c0d3a160a759e11
Benny <peptoni0345@gmail.com>
2025-08-15 14:39:11 +0200

    refactor: remove manifest function, update environment validation, and simplify build scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,25 +8,20 @@ import ThemeProvider from '~/providers/theme-provider'
 import { Toaster } from '~/components/ui/sonner'
 import Script from 'next/script'
 import FirstTimeVisitScript from '~/scripts/first-time-visit'
+import { env } from '~/env'
 
 export const metadata: Metadata = {
     title: 'Pomodoro Timer',
     description: 'Simple pomodoro timer',
-    icons: [
-        { rel: 'icon', type: 'shortcut icon', url: '/favicon.ico' },
-        { rel: 'icon', url: '/favicon.ico' },
-        { rel: 'icon', type: 'image/svg+xml', url: '/favicon.svg' },
-        { rel: 'icon', type: 'image/png', url: '/favicon-96x96.png', sizes: '96x96' },
-        { rel: 'apple-touch-icon', sizes: '180x180', url: '/apple-icon.png' },
-    ],
 }
 
 export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
     return (
         <html suppressHydrationWarning lang="es" className={`${GeistSans.variable}`}>
             <head>
-                <meta name="apple-mobile-web-app-title" content="Pomodoro" />
-                <Script crossOrigin="anonymous" src="//unpkg.com/react-scan/dist/auto.global.js" />
+                {env.NEXT_PUBLIC_ENV === 'dev' && (
+                    <Script crossOrigin="anonymous" src="//unpkg.com/react-scan/dist/auto.global.js" />
+                )}
                 <FirstTimeVisitScript />
                 <title>Pomodoro Timer</title>
             </head>
