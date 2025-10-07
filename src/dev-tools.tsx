82ec9e4902f355ec2c666a90e1f3a855a122b6b5
Benny <peptoni0345@gmail.com>
2025-10-07 21:31:19 +0200

    feat: pwa

--- src/dev-tools.tsx
+++ src/dev-tools.tsx
@@ -1,14 +1,16 @@
 import { TanStackDevtools } from '@tanstack/react-devtools'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { FormDevtoolsPlugin } from '@tanstack/react-form-devtools'
 
 export default function Devtools() {
   return (
     <>
-      <script crossOrigin="anonymous" src="//unpkg.com/react-scan/dist/auto.global.js" />
+      <script crossOrigin="anonymous" src="//unpkg.com/react-scan/dist/auto.global.js"></script>
       <TanStackDevtools
         plugins={[
+          FormDevtoolsPlugin(),
           {
-            name: 'Tanstack Router',
+            name: 'TanStack Router',
             render: <TanStackRouterDevtoolsPanel />,
           },
         ]}
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,69 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import tanstackRouter from '@tanstack/router-plugin/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  preview: { allowedHosts: true },
   server: { port: 3000 },
-  plugins: [tanstackRouter({ target: 'react', autoCodeSplitting: true }), tailwindcss(), viteReact(), tsconfigPaths()],
+  plugins: [
+    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
+    tailwindcss(),
+    viteReact(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+        screenshots: [
+          {
+            src: 'screenshot-desktop.png',
+            sizes: '1053x593',
+            type: 'image/png',
+            form_factor: 'wide',
+          },
+          {
+            src: 'screenshot-mobile.png',
+            sizes: '406x895',
+            type: 'image/png',
+            form_factor: 'narrow',
+          },
+          {
+            src: 'screenshot-mobile.png',
+            sizes: '406x895',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*'],
+        maximumFileSizeToCacheInBytes: 30000000,
+      },
+    }),
+  ],
 })
